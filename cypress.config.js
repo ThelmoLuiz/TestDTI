5d5e8f9f89f78d5867cbff2a17bf503befa37505
ThelmoLuiz <89103360+ThelmoLuiz@users.noreply.github.com>
2025-01-27 01:27:52 -0300

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+module.exports = defineConfig({
+  e2e: {
+    baseUrl:"http://www.automationpractice.pl/index.php?controller=my-account",
+    specPattern:
+      "C:Users\thelmo.junior_sankhyDesktopCypresscypresse2e\finances.spec.js",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
